5563e2581b7bc7f240946a6eeb7f720f7f513dc8
Yulia Dolbina <yu.dolbina@yandex.ru>
2024-09-26 13:25:51 +0400

    file

--- test_string_utils.py
+++ test_string_utils.py
@@ -1,3 +1,4 @@
+import pytest
 from string_utils import StringUtils
 
 def test_capitilize():
@@ -10,8 +11,30 @@ def test_trim():
     whitespace = " "
     while (string.startswith(whitespace)):
         string = string.removeprefix(whitespace)
-        res = string(' word')
+        res = string.trim(' word')
         assert res == 'word'
+
+def test_to_list():
+    string = StringUtils()
+    res = string.to_list("ab,cd,ef,gh")
+    assert res == ["ab", "cd", "ef", "gh"]
+
+@pytest.mark.parametrize ('word, letter', [("Apple", "a"), ("House", "b")])
+def test_contains(word, letter):
+    string = StringUtils()
+    res = string.contains(word, letter)
+    assert res == False
+
+@pytest.mark.parametrize ('word, symbol', [("apple", "a"), ("apple", "e")])
+def test_delete_symbol(word, symbol):
+    string = StringUtils()
+    res = string.delete_symbol(word, symbol)
+    assert res == res
+
+
+
+
+
 
 
 
