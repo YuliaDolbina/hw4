d1e8ef56c6d41cce379dbef2d22b6681ac2756db
Yulia Dolbina <yu.dolbina@yandex.ru>
2025-01-21 11:17:30 +0400

    adding

--- controlnaya/test_task2.py
+++ controlnaya/test_task2.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
