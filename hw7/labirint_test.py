f9025c10222eacd5e977bfde1425bc5d94cc8dde
Yulia Dolbina <yu.dolbina@yandex.ru>
2025-01-30 12:27:56 +0400

    fixed

--- hw7/labirint_test.py
+++ hw7/labirint_test.py
@@ -1,31 +1,62 @@
-from time import sleep
 from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 cookie = {"name": "cookie_policy",
           "value": "1"}
 
-def test_cart_counter():
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+browser = browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+def open_labirint ():
     browser.get("https://www.labirint.ru")
     browser.implicitly_wait(4)
     browser.maximize_window()
     browser.add_cookie(cookie)
 
-    browser.find_element(By.CSS_SELECTOR, "#search-field").send_keys("python")
+def search(term):
+    browser.find_element(By.CSS_SELECTOR, "#search-field").send_keys(term)
     browser.find_element(By.CSS_SELECTOR, "#searchform > div.b-search-e-input-wrapper > button > span.b-header-b-search-e-btntxt").click()
 
-    buy_buttons = browser.find_element(By.CSS_SELECTOR, "a._btn.btn-tocart.buy-link")
+def add_books():
+    buy_buttons = browser.find_element(By.CSS_SELECTOR, "._btn.btn-tocart.buy-link")
+    counter = 0
+    for btn in buy_buttons:
+        btn.click()
+        counter += 1
 
-    print(len(buy_buttons))
+    return counter 
 
-    sleep(5)
+def go_to_cart():
+    browser.get("https://www.labirint.ru/cart/")
 
+def get_cart_count():
+    txt = browser.find_element(By.CSS_SELECTOR, "#basket-default-prod-count2").text
+    return int(txt)
+
+def close_browser():
     browser.quit()
+
+def test_cart_counter():
+
+    open_labirint()
+
+    search('python')
+
+    added = add_books()
+
+    go_to_cart()
+
+    cart_count = get_cart_count()
+
+    close_browser()
+
+    assert added == cart_count
+    
+
+    
 
 
         
