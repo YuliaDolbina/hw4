ac8f887bc5e18a43ba488aec9b208fb0a92b8b00
Yulia Dolbina <yu.dolbina@yandex.ru>
2025-02-06 18:15:26 +0400

    added

--- hw8/dict.py
+++ hw8/dict.py
@@ -1,3 +1,5 @@
+import pytest
+
 empty_dict = {}
 
 
@@ -37,5 +39,43 @@ def test_write_new_value():
     assert winner == "Аргентина"
 
 
+def test_read_list():
+    participants = football_stats["Участники"]
+    england = football_stats["Участники"] [1]
 
-    
+    assert len(participants) > 0 
+    assert participants[0] == "Австралия"
+    assert england == "Англия"
+
+
+def test_read_dict():
+    awards = football_stats["Награды"] ["Золотая бутса"]
+
+    assert awards == "Килиан Мбаппе" 
+
+
+def test_read_dict1():
+    awards1 = football_stats["Награды"] ["Больше всего голов"] ["Количество мячей"]
+
+    assert awards1 == 8
+
+
+def test_save_dict():
+
+    awards2 = football_stats["Награды"] 
+    player = awards2["Больше всего голов"]["Игрок"]
+
+    assert player == "Килиан Мбаппе - капитан команды"
+
+
+def test_read_error():
+    with pytest.raises(KeyError):
+        empty_dict["key"]
+
+def test_get_empty():
+    value = empty_dict.get("key")
+    assert value == None
+
+def test_get_empty_or_default():
+    value = empty_dict.get("key", "abc123")
+    assert value == "abc123"
