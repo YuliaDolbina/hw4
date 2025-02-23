160425b55debe2c69f2b630cc067a1cda77370e1
Yulia Dolbina <yu.dolbina@yandex.ru>
2025-02-23 17:52:09 +0400

    added

--- hw9/EmployeeTable.py
+++ hw9/EmployeeTable.py
@@ -1,24 +1,45 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 class EmployeeTable:
     def __init__(self, connection_string):
         self.db = create_engine(connection_string)
-        Base = declarative_base()
-        session = sessionmaker(bind=self.db, expire_on_commit=False)
+        self.session = sessionmaker(bind=self.db, expire_on_commit=False)
 
+    def create_employee(
+            self,
+            first_name,
+            last_name,
+            middle_name,
+            company_id,
+            email,
+            phone,
+            birthdate,
+            is_active,
+    ):
+        with self.session() as session:
+            created_employee = session.execute(
+                text(
+                    f"""
+                    insert into employee (first_name, last_name, middle_name, company_id, email, phone, birthdate, is_active)
+                    values ('{first_name}', '{last_name}', '{middle_name}',
+                    {company_id}, '{email}', '{phone}', '{birthdate}', {is_active})
+                    returning id, first_name, last_name, middle_name, company_id, email, phone, birthdate, is_active
+                    """
+                ),
+            ).fetchone()
+            session.commit()
 
-    def create_employee(self):
-        return self.db.session().execute(text("insert into employee (fist_name, last_name, middle_name, company_id, email, phone, birthdate, is_active) values (:first_name, :last_name, :middle_name, :company_id, :email, :phone, :birthdate, :is_active)")).fetchall()
-    
+            return created_employee
 
     def get_info(self, id):
-        return self.db.session().execute(text(f"select max{id} from employee")).fetchall()
-    
+        return self.session().execute(text(f"select max({id}) from employee")).fetchone()
 
     def delete(self, id):
-        return self.db.session().execute(text(f"delete from employee where id = {id}"))
-
+        return self.session().execute(text(f"delete from employee where id = {id}"))
+    
+    def get_list_company_id(self, company_id):
+        return self.session().execute(text(f"select * from employee where company_id = {company_id}")).fetchone()
 
 ## pytest -v hw9/test_employee_db.py
