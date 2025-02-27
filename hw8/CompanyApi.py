97c4b99425a426b6316849b5b0aaab2a4e44cf11
Yulia Dolbina <yu.dolbina@yandex.ru>
2025-02-27 10:06:58 +0400

    added

--- hw8/CompanyApi.py
+++ hw8/CompanyApi.py
@@ -1,18 +1,22 @@
+import allure
 import requests
 
 class CompanyApi:
     def __init__(self, url):
         self.url = url
 
+
+    @allure.step("Getting the list of companies")
     def get_company_list(self, params_to_add = None):
         resp = requests.get(self.url + '/company/list', params=params_to_add)
         return resp.json()
     
-
+    @allure.step("Getting the company using id")
     def get_company(self, id):
         resp = requests.get(self.url + '/company/'+str(id))
         return resp.json()
     
+    @allure.step("Auth")
     def get_token(self, user = 'harrypotter', password = 'expelliarmus'):
         creds = {
             'username': user, 
@@ -21,7 +25,7 @@ class CompanyApi:
         resp = requests.post(self.url +'/auth/login', json=creds)
         return resp.json()["user_token"]
 
-
+    @allure.step("Adding a company")
     def create_company(self, name, description=""):
         company = {
         "name": name,
@@ -30,7 +34,7 @@ class CompanyApi:
         resp = requests.post(self.url + "/company/create", json=company)
         return resp.json()    
 
-
+    @allure.step("Editing a company")
     def edit_company(self,new_id, new_name, new_descr):
         client_token = self.get_token()
         url_with_token = f"{self.url}/company/update/{new_id}?client_token={client_token}"
@@ -42,7 +46,7 @@ class CompanyApi:
         resp = requests.patch(url_with_token, json=company)
         return resp.json()
 
-
+    @allure.step("Deleting a company")
     def delete(self, id):
         client_token = self.get_token()
         url_with_token = f"{self.url}/company/{id}?client_token={client_token}"
